f41fcaeb96fee53de0337203aa242f37b5497079
Proton <feisuzhu@163.com>
2022-07-06 17:02:11 +0800

    feat: mse -> rmse, compare utility, threshold adjustments

--- actions/capture.py
+++ actions/capture.py
@@ -18,15 +18,39 @@ from exceptions import Failed
 # -- code --
 parser.add_argument('--generate-captures', action='store_true')
 
+# def _get_gaussian_coef(radius):
+#     from math import erfc
+#     a = 3.0 / radius * 0.707106781
+#     f = lambda x: 0.5*erfc(-x*a)
+#     l = [f(0.5 + i) - f(-0.5 + i) for i in range(radius+1)]
+#     l = [i for i in l if i>0.01]
+#     l = list(reversed(l[1:])) + l
+#     s = sum(l)
+#     l = [i/s for i in l]
+#     return l
+
+
+GAUSSIAN_COEFF = [
+    0.01449797497581252,
+    0.04928451699227458,
+    0.11807162656393803,
+    0.19941115896256947,
+    0.23746944501081074,
+    0.19941115896256947,
+    0.11807162656393803,
+    0.04928451699227458,
+    0.01449797497581252,
+]
+
 
 @ti.kernel
-def mse(a: ti.template(), b: ti.template()) -> ti.f32:
+def rmse(a: ti.template(), b: ti.template()) -> ti.f32:
     assert a.shape == b.shape
     acc: ti.f32 = 0
     for i, j in a:
         v = a[i, j] - b[i, j]
         acc += (v * v).sum()
-    return acc / a.shape[0] / a.shape[1]
+    return ti.sqrt(acc / a.shape[0] / a.shape[1])
 
 
 @ti.kernel
@@ -48,6 +72,22 @@ def pixel_count(a: ti.template(), b: ti.template()) -> ti.i32:
         if any(a[i, j] != b[i, j]):
             acc += 1
     return acc
+
+
+@ti.kernel
+def gaussian_blur(a: ti.template(), aux: ti.template()):
+    assert a.shape == aux.shape
+    for i, j in a:
+        acc = ti.Vector([0.0, 0.0, 0.0])
+        for k in ti.static(range(-4, 5)):
+            acc += GAUSSIAN_COEFF[k + 4] * a[i, j+k]
+        aux[i, j] = ti.cast(acc, ti.i16)
+
+    for i, j in a:
+        acc = ti.Vector([0.0, 0.0, 0.0])
+        for k in ti.static(range(-4, 5)):
+            acc += GAUSSIAN_COEFF[k + 4] * aux[i+k, j]
+        a[i, j] = ti.cast(acc, ti.i16)
 
 
 # -- code --
@@ -87,11 +127,18 @@ def capture_and_compare(dry, gui, compare, ground_truth, threshold):
     f_truth.from_numpy(truth[:, :, :3])
 
     pixels = f_captured.shape[0] * f_captured.shape[1]
-    if compare == 'mse':
-        diff = mse(f_captured, f_truth)
+    if compare == 'rmse':
+        diff = rmse(f_captured, f_truth)
         if isinstance(threshold, str) and threshold.endswith('%'):
             threshold = float(threshold[:-1]) / 100 * 255
     elif compare == 'sum-difference':
+        diff = sum_difference(f_captured, f_truth)
+        if isinstance(threshold, str) and threshold.endswith('%'):
+            threshold = float(threshold[:-1]) / 100 * pixels * 3 * 255
+    elif compare == 'blur-sum-difference':
+        f_aux = ti.Vector.field(3, dtype=ti.i16, shape=truth.shape[:2])
+        gaussian_blur(f_captured, f_aux)
+        gaussian_blur(f_truth, f_aux)
         diff = sum_difference(f_captured, f_truth)
         if isinstance(threshold, str) and threshold.endswith('%'):
             threshold = float(threshold[:-1]) / 100 * pixels * 3 * 255
---
+++ compare.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# -- stdlib --
+import argparse
+
+# -- third party --
+import taichi as ti
+
+# -- own --
+from actions.capture import gaussian_blur, rmse, pixel_count, sum_difference
+
+
+# -- code --
+def main():
+    ti.init(arch=ti.gpu)
+
+    parser = argparse.ArgumentParser('compare')
+    parser.add_argument('a')
+    parser.add_argument('b')
+    options = parser.parse_args()
+
+    file_a = ti.tools.imread(options.a)
+    file_b = ti.tools.imread(options.b)
+
+    assert file_a.shape == file_b.shape
+
+    f_a = ti.Vector.field(3, dtype=ti.i16, shape=file_a.shape[:2])
+    f_b = ti.Vector.field(3, dtype=ti.i16, shape=file_b.shape[:2])
+    f_aux = ti.Vector.field(3, dtype=ti.i16, shape=file_a.shape[:2])
+    f_a.from_numpy(file_a[:, :, :3])
+    f_b.from_numpy(file_b[:, :, :3])
+
+    pixels = f_a.shape[0] * f_a.shape[1]
+    diff = rmse(f_a, f_b)
+    print(f'rmse: {diff}')
+    diff = sum_difference(f_a, f_b)
+    print(f'sum difference: {diff}, {diff / (pixels * 3 * 255) * 100:.2f}%')
+    diff = pixel_count(f_a, f_b)
+    print(f'pixel count: {diff}, {diff / pixels * 100:.2f}%')
+    gaussian_blur(f_a, f_aux)
+    gaussian_blur(f_b, f_aux)
+    diff = sum_difference(f_a, f_b)
+    print(f'blur sum difference: {diff}, {diff / (pixels * 3 * 255) * 100:.2f}%')
+
+if __name__ == '__main__':
+    main()
